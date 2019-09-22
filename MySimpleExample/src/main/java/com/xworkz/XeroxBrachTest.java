e460385f6172a003841269089a8865c5cf355389
Shruthi-ns <shruthins19@gmail.com>
2019-09-22 11:31:57 +0530

    This is my new comit for today

--- MySimpleExample/src/main/java/com/xworkz/XeroxBrachTest.java
+++ MySimpleExample/src/main/java/com/xworkz/XeroxBrachTest.java
@@ -5,6 +5,9 @@ public class XeroxBrachTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.println("hello");
+		
+		System.out.println("x-workzzzzzzzzzzzzzz");
 	}
 
 }
